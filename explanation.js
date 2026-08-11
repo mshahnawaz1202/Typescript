f132b0373d1e95181f83cd175b71d5ac6c397b51
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-11 13:13:41 +0500

    Add explanation for using TypeScript with JavaScript libraries

---
+++ explanation.js
@@ -0,0 +1,22 @@
+/**
+ * ============================================================================
+ * ? TypeScript with JavaScript Libraries
+ * ============================================================================
+ *
+ * * Some JavaScript libraries do not include TypeScript type definitions.
+ *
+ * * When using such a library:
+ *
+ * * 1. Install the library normally.
+ *
+ *  npm i some-library
+ *
+ * * 2. Install its TypeScript type definitions as a development dependency.
+ *
+ *  npm i -D @types/some-library
+ *
+ * * The `@types` package provides TypeScript type definitions
+ * * for JavaScript libraries.
+ *
+ * ============================================================================
+ */
