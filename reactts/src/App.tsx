9165d4f65dbe07a6fecc0cfac03b1457329727c9
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-11 17:19:35 +0500

    feat: initialize React TypeScript project with Vite
    - Added main application files including App.tsx and App.css.
    - Created Card component for displaying product information.
    - Included assets: hero image and SVG logos for React and Vite.
    - Set up global CSS variables and styles in index.css.
    - Configured TypeScript with appropriate settings in tsconfig files.
    - Established Vite configuration for building the project.

---
+++ reactts/src/App.tsx
@@ -0,0 +1,21 @@
+import Card from './components/Card.tsx'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <h1>Vite + React</h1>
+      <Card
+      name = "Headphone"
+      price = {5000}
+
+      />
+    </div>
+    
+    </>
+  )
+}
+
+export default App
