e5a6f4b05cf152fa49d6668114318d567411cc10
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-11 13:25:59 +0500

    Add fetchData function to retrieve Todo data using axios

---
+++ src/14_webReq.ts
@@ -0,0 +1,35 @@
+import axios,{type AxiosResponse} from 'axios'
+
+interface Todo{
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
+
+const fetchData = async() =>{
+    try {
+        const response : AxiosResponse<Todo> = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        console.log("Todo : ", response.data);
+        
+    } catch (error:any) {
+        if(axios.isAxiosError(error)){
+            console.log(error.message);
+            
+        }
+        
+        
+    }
+}
+
+
+
+
+
+
+
+// axios.get('https://example.com/data')
+// .then(response => {
+//     console.log(response.data);
+    
+// })
