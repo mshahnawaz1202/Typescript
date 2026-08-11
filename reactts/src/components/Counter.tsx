f370275ec3e05b07b4652f50cadff27dca51de6b
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-11 18:33:34 +0500

    fix: ensure count state in Counter component handles null values

--- reactts/src/components/Counter.tsx
+++ reactts/src/components/Counter.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
 
 export function Counter(){
-    const [count, setcount] = useState(0)
+    const [count, setcount] = useState<number | null>(0)
     return(
         <div>
             <p>Cups Ordered : {count} </p>
             <button
-            onClick={() => setcount((c) => c+1)}
+            onClick={() => setcount((c) => (c ?? 0)+1)}
             >Order</button>
         </div>
     )
