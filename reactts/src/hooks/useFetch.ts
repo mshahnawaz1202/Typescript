d13d168643a3a91a56fb70e61454b887c3582965
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-08-11 20:04:21 +0500

    feat: implement useFetch hook for data fetching in React

---
+++ reactts/src/hooks/useFetch.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+interface fetchState<T> {
+    data: T | null,
+    loading: boolean,
+    error: string | null
+}
+
+export function useFetch<T>(url: string): fetchState<T> {
+    const [state, setState] = useState<fetchState<T>>({
+        data: null,
+        loading: true,
+        error: null
+    })
+
+    //* useEffect to make fetch request
+
+
+
+
+
+    return state
+
+}
